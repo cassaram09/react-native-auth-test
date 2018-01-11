8641dec597e3e54175af6a1def6f63a52c3d9ed3
cassaram09 <cassaram09@gmail.com>
2018-01-11 08:30:46 -0500

    start building login form

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/loginForm';
 
 class App extends Component<{}> {
 
-  componentWillMount(){
+  componentWillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyBu0Hi6qsOTmHU-_i2ITI5fl9oF9elVjLo",
-      authDomain: "react-native-auth-test-e9421.firebaseapp.com",
-      databaseURL: "https://react-native-auth-test-e9421.firebaseio.com",
-      projectId: "react-native-auth-test-e9421",
-      storageBucket: "react-native-auth-test-e9421.appspot.com",
-      messagingSenderId: "796274887830"
+      apiKey: 'AIzaSyBu0Hi6qsOTmHU-_i2ITI5fl9oF9elVjLo',
+      authDomain: 'react-native-auth-test-e9421.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-test-e9421.firebaseio.com',
+      projectId: 'react-native-auth-test-e9421',
+      storageBucket: 'react-native-auth-test-e9421.appspot.com',
+      messagingSenderId: '796274887830'
     });
   }
 
   render() {
     return (
       <View>
-        <Header headerText='Authentication'/>
-        <Text>
-          Welcome to React Native!
-        </Text>
+        <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   }
